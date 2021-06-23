26211eb0f98b2dca8ff6b15c4787d77b7d8f1437
OH-Seoyoung <eileen1426@naver.com>
2021-06-24 00:56:16 +0900

    20210624

--- Lenet/lenet.py
+++ Lenet/lenet.py
@@ -4,7 +4,7 @@ from keras.layers.convolutional import MaxPooling2D
 from keras.layers.core import Activation, Flatten, Dense
 
 class LeNet : 
-    def build(input_shape, classes):
+    def build(input_shape, classes, weightsPath = None):
         model = Sequential()
         model.add(Conv2D(20, kernel_size = 5, padding="same", input_shape=input_shape))
         model.add(Activation("relu"))
@@ -15,6 +15,10 @@ class LeNet :
         model.add(Flatten())
         model.add(Dense(500))
         model.add(Activation("relu"))
-        model.add(Dense(2))
-        model.add(Activation("sigmoid"))
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+        
+        if weightsPath is not None:
+            model.load_weights(weightsPath)
+            
         return model
